e8c837b1805996ebef0c81bb1acdf4a777139bee
Sam McEwan <samlmcewan@gmail.com>
2023-12-28 19:12:42 +0000

    bash n yml

--- process_dsl.sh
+++ process_dsl.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-echo "Running structurizr-cli command..."
 find . -name "*.dsl" -exec structurizr-cli export -i {} -o /workspace/mermaid/ --format mermaid \;
-
-echo "Contents of mermaid/ directory:"
-ls -la /workspace/mermaid/
-
-echo "Contents of Mermaid files:"
-cat /workspace/mermaid/*.mermaid
